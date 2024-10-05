7cc7dfff8ba378f4e3cbec12d21609e6e8455d3f
David Requeno <davidrequeno52@gmail.com>
2024-10-05 15:52:18 -0600

    init commit

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,4 +1,7 @@
+import { ChatWrapper } from "@/components/ChatWrapper";
 import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
 
 interface PageProps {
   params: {
@@ -11,19 +14,41 @@ function reconstructUrl({ url }: { url: string[] }) {
     decodeURIComponent(component)
   );
 
-  return decodedComponents.join("/");
+  return decodedComponents.join("//");
 }
 
 const Page = async ({ params }: PageProps) => {
-  const reconstructuredUrl = reconstructUrl({ url: params.url as string[] });
+  const sessionCookie = cookies().get("sessionId")?.value;
+  const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
 
-  await ragChat.context.add({
-    type: "html",
-    source: reconstructuredUrl,
-    config: { chunkOverlap: 50, chunkSize: 200 },
+  const sessionId = (reconstructedUrl + "--" + sessionCookie).replace(
+    /\//g,
+    ""
+  );
+
+  const isAlreadyIndexed = await redis.sismember(
+    "indexed-urls",
+    reconstructedUrl
+  );
+
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId,
   });
 
-  return <div>page</div>;
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: reconstructedUrl,
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    });
+
+    await redis.sadd("indexed-urls", reconstructedUrl);
+  }
+
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
 };
 
 export default Page;
